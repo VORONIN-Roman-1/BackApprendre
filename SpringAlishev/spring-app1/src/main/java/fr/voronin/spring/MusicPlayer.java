dc7ddbbbb4db5c89e0eda6e63a1e15c5c64d153f
VORONIN Roman <voronin.roman.i@gmail.com>
2022-02-21 16:14:40 +0100

    DI

--- SpringAlishev/spring-app1/src/main/java/fr/voronin/spring/MusicPlayer.java
+++ SpringAlishev/spring-app1/src/main/java/fr/voronin/spring/MusicPlayer.java
@@ -2,10 +2,40 @@ package fr.voronin.spring;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private  int volume;
+
+    public MusicPlayer() {
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public  void  playMusic(){
         System.out.println("Playing "+ music.getSong());
     }
