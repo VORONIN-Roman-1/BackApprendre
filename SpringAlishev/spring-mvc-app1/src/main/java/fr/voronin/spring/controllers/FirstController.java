53a77ad59d206686ceedfedd3d35f56c9bce5f56
VORONIN Roman <voronin.roman.i@gmail.com>
2022-02-23 10:19:56 +0100

    JDBC

---
+++ SpringAlishev/spring-mvc-app1/src/main/java/fr/voronin/spring/controllers/FirstController.java
@@ -0,0 +1,26 @@
+package fr.voronin.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+//@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name",required = false) String name,
+                            @RequestParam (value = "surname", required = false) String surname,
+                            Model model
+    ){
+        model.addAttribute("message", "Hello "+name+" "+surname);
+        return "first/hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodBayPage(){
+        return "first/goodbye";
+    }
+}
