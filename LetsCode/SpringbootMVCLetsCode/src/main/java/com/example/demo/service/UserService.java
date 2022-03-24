3b5adff18e41f10b0774b66587082d0ff84bd347
VORONIN Roman <voronin.roman.i@gmail.com>
2022-03-24 20:35:38 +0100

    letsCod MVC 10 FlyWay

--- LetsCode/SpringbootMVCLetsCode/src/main/java/com/example/demo/service/UserService.java
+++ LetsCode/SpringbootMVCLetsCode/src/main/java/com/example/demo/service/UserService.java
@@ -9,8 +9,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -39,14 +39,19 @@ public class UserService implements UserDetailsService {
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
         userRepository.save(user);
-        String message = String.format(
-                "Hello, %s!\n" +
-                        "Wilcome to Sweater. Please visit netx link: http://localhost:8080/activate/%s", user.getUsername(), user.getActivationCode()
-        );
-        if (!user.getEmail().isBlank()) {
+
+        sendMessage(user);
+        return true;
+    }
+
+    private void sendMessage(User user) {
+        if (user.getEmail() != null && !user.getEmail().isBlank()) {
+            String message = String.format(
+                    "Hello, %s!\n" +
+                            "Wilcome to Sweater. Please visit netx link: http://localhost:8080/activate/%s", user.getUsername(), user.getActivationCode()
+            );
             mailSenderService.send(user.getEmail(), "Activation code", message);
         }
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -57,5 +62,42 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(null);
         userRepository.save(user);
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void save(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+        boolean isEmailChanged = ((email != null && !email.equals(userEmail)) || (userEmail != null && !userEmail.equals(email)));
+        if (isEmailChanged) {
+            user.setEmail(email);
+            if (email != null && !email.isBlank()) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+        if (password != null && !password.isBlank()) {
+            user.setPassword(password);
+        }
+        userRepository.save(user);
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
     }
 }
